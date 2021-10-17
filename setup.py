ef6aa6207182ea84c050325d4e1751ac8908598a
Martmists <martmists@gmail.com>
2021-10-17 12:11:43 +0200

    Add readme
    Signed-off-by: Martmists <martmists@gmail.com>

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pymixin",
-    version="1.1.0",
+    version="1.1.1",
     author="Martmists",
     author_email="martmists@gmail.com",
     description="A high-level library for manipulating Python Bytecode in an easy way.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     classifiers=[
-        "Development Status :: 5 - Stable",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Application Frameworks",
         "Topic :: Software Development :: Code Generators",
